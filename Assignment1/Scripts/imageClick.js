eab8fd1f30c15255cbe7721fccc77a242691e863
mmoya26 <mmoya18@icloud.com>
2020-01-30 18:16:23 -0600

    Create imgClick container + Add initial javascript for images

--- Assignment1/Scripts/imageClick.js
+++ Assignment1/Scripts/imageClick.js
@@ -1,1 +1,11 @@
 ﻿console.log("Buenas")
+
+
+var imgElement = document.createElement('img');
+var imageOne = document.getElementById('image-one');
+var imageTwo = document.getElementById('image-two');
+var imageThree = document.getElementById('image-three');
+
+console.log(imageOne)
+console.log(imageTwo)
+console.log(imageThree)
