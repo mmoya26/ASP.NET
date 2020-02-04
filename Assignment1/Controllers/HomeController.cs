0a43b887a5983b6aa39f07909763a68ca944091b
mmoya26 <mmoya18@icloud.com>
2020-02-04 14:22:21 -0600

    Create flower class + start flowers build

--- Assignment1/Controllers/HomeController.cs
+++ Assignment1/Controllers/HomeController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assignment1.Helpers;
 
 namespace Assignment1.Controllers
 {
     public class HomeController : Controller
     {
+
+        List<Flower> flowerList = new List<Flower>();
+        Flower firstFlower = new Flower("Aster Novae Angliae", "red", ["Bright Colors", "Strong Living", "Made for Inside"]);
+        Flower secondFlower = new Flower("Aster Pantens", "red", ["Bright Colors", "Strong Living", "Made for Inside"]);
+        Flower thirdFlower = new Flower("Centaurea Cyanus Peg", "red", ["Bright Colors", "Strong Living", "Made for Inside"]);
+
         public ActionResult Index()
         {
+            flowerList.Add(firstFlower);
+            flowerList.Add(secondFlower);
+            flowerList.Add(thirdFlower);
+
+            ViewBag.flowerList = flowerList;
+
             return View();
         }
 
---
+++ Assignment1/Helpers/Flower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Helpers
+{
+    public class Flower
+    {
+        public string name;
+        public string color;
+        public string[] descriptions;
+
+
+        public Flower(string name, string color) {
+            this.name = name;
+            this.color = color;
+        }
+
+        public Flower(string name, string color, string[] descriptions)
+        {
+            this.name = name;
+            this.color = color;
+            this.descriptions = descriptions;
+        }
+
+        public string converFlowerNameToPath(string flowerName) {
+
+            string newString = flowerName;
+            newString.ToLower();
+            newString.Replace(" ", "_");
+
+            return newString;
+        }
+    }
+}
